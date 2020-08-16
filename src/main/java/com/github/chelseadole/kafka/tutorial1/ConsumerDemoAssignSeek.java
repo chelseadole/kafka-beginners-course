73bed61d3e771bfa762dfae68fdd843f294a7543
Chelsea Dole <chelsea@coffeemeetsbagel.com>
2020-08-16 21:59:28 +0100

    add final consumer, with assign seek.

---
+++ src/main/java/com/github/chelseadole/kafka/tutorial1/ConsumerDemoAssignSeek.java
@@ -0,0 +1,64 @@
+package com.github.chelseadole.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoAssignSeek {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignSeek.class.getName());
+
+        String bootstrapServer = "127.0.0.1:9092";
+        String topic = "not-mow";
+
+        // Create consumer configuration
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // "earliest", "latest", or "none"
+
+        // Create the consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // assign and seek is mostly used to replay data, or fetch a specific message
+        TopicPartition topicPartitionToReadFrom = new TopicPartition(topic, 0);
+        long offsetToReadFrom = 15L;
+        consumer.assign(Arrays.asList(topicPartitionToReadFrom));
+
+        // Seek. "this consumer should read this partition, offset #15
+        consumer.seek(topicPartitionToReadFrom, offsetToReadFrom);
+
+        int numberOfMessagesToRead = 5;
+        int numberOfMessagesReadSoFar = 0;
+        boolean keepOnReading = true;
+
+        // Poll for new data! :D
+        while (keepOnReading) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                numberOfMessagesReadSoFar += 1;
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition());
+                if (numberOfMessagesReadSoFar >= numberOfMessagesToRead) {
+                    keepOnReading = false; // to exit the while loop
+                    break; // to exit the for loop
+                }
+            }
+        }
+
+        logger.info("Exiting the application...");
+    }
+}
